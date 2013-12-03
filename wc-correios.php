d0d963650ee03550935fee3afdf95d36b739972e
claudiosanches <claudiosanches@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-03 15:59:23 +0000

    updated to version 1.6.0
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-correios/trunk@814447 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wc-correios.php
+++ wc-correios.php
@@ -5,7 +5,7 @@
  * Description: Correios para WooCommerce
  * Author: claudiosanches, rodrigoprior
  * Author URI: http://claudiosmweb.com/
- * Version: 1.5.0
+ * Version: 1.6.0
  * License: GPLv2 or later
  * Text Domain: wccorreios
  * Domain Path: /languages/
@@ -18,14 +18,14 @@ define( 'WOO_CORREIOS_URL', plugin_dir_url( __FILE__ ) );
  * WooCommerce fallback notice.
  */
 function wccorreios_woocommerce_fallback_notice() {
-    echo '<div class="error"><p>' . sprintf( __( 'WooCommerce Correios depends on %s to work!', 'wccorreios' ), '<a href="http://wordpress.org/extend/plugins/woocommerce/">WooCommerce</a>' ) . '</p></div>';
+	echo '<div class="error"><p>' . sprintf( __( 'WooCommerce Correios depends on %s to work!', 'wccorreios' ), '<a href="http://wordpress.org/extend/plugins/woocommerce/">WooCommerce</a>' ) . '</p></div>';
 }
 
 /**
  * SimpleXML missing notice.
  */
 function wccorreios_extensions_missing_notice() {
-    echo '<div class="error"><p>' . sprintf( __( 'WooCommerce Correios depends to %s to work!', 'wccorreios' ), '<a href="http://php.net/manual/en/book.simplexml.php">SimpleXML</a>' ) . '</p></div>';
+	echo '<div class="error"><p>' . sprintf( __( 'WooCommerce Correios depends to %s to work!', 'wccorreios' ), '<a href="http://php.net/manual/en/book.simplexml.php">SimpleXML</a>' ) . '</p></div>';
 }
 
 /**
@@ -33,66 +33,44 @@ function wccorreios_extensions_missing_notice() {
  */
 function wccorreios_shipping_load() {
 
-    if ( ! class_exists( 'WC_Shipping_Method' ) ) {
-        add_action( 'admin_notices', 'wccorreios_woocommerce_fallback_notice' );
+	if ( ! class_exists( 'WC_Shipping_Method' ) ) {
+		add_action( 'admin_notices', 'wccorreios_woocommerce_fallback_notice' );
 
-        return;
-    }
+		return;
+	}
 
-    if ( ! class_exists( 'SimpleXmlElement' ) ) {
-        add_action( 'admin_notices', 'wccorreios_extensions_missing_notice' );
+	if ( ! class_exists( 'SimpleXmlElement' ) ) {
+		add_action( 'admin_notices', 'wccorreios_extensions_missing_notice' );
 
-        return;
-    }
+		return;
+	}
 
-    /**
-     * Load textdomain.
-     */
-    load_plugin_textdomain( 'wccorreios', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+	/**
+	 * Load textdomain.
+	 */
+	load_plugin_textdomain( 'wccorreios', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
 
-    /**
-     * Add the Correios to shipping methods.
-     *
-     * @param array $methods
-     *
-     * @return array
-     */
-    function wccorreios_add_method( $methods ) {
-        $methods[] = 'WC_Correios';
+	/**
+	 * Add the Correios to shipping methods.
+	 *
+	 * @param array $methods
+	 *
+	 * @return array
+	 */
+	function wccorreios_add_method( $methods ) {
+		$methods[] = 'WC_Correios';
 
-        return $methods;
-    }
+		return $methods;
+	}
 
-    add_filter( 'woocommerce_shipping_methods', 'wccorreios_add_method' );
+	add_filter( 'woocommerce_shipping_methods', 'wccorreios_add_method' );
 
-    // WC_Correios class.
-    include_once WOO_CORREIOS_PATH . 'includes/class-wc-correios.php';
+	// WC_Correios class.
+	include_once WOO_CORREIOS_PATH . 'includes/class-wc-correios.php';
 
-    // Metabox.
-    include_once WOO_CORREIOS_PATH . 'includes/class-wc-correios-tracking.php';
-    $wc_correios_metabox = new WC_Correios_Tracking;
+	// Metabox.
+	include_once WOO_CORREIOS_PATH . 'includes/class-wc-correios-tracking.php';
+	$wc_correios_metabox = new WC_Correios_Tracking;
 }
 
 add_action( 'plugins_loaded', 'wccorreios_shipping_load', 0 );
-
-/**
- * Adds custom settings url in plugins page.
- *
- * @param  array $links Default links.
- *
- * @return array        Default links and settings link.
- */
-function wccorreios_action_links( $links ) {
-
-    $settings = array(
-        'settings' => sprintf(
-            '<a href="%s">%s</a>',
-            admin_url( 'admin.php?page=woocommerce_settings&tab=shipping&section=WC_Correios' ),
-            __( 'Settings', 'wccorreios' )
-        )
-    );
-
-    return array_merge( $settings, $links );
-}
-
-add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), 'wccorreios_action_links' );
